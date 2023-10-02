fbbfb40fa36b8c67f25066899e285b5cacbedaf6
fadilkun45 <indocrafter76@gmail.com>
2023-10-02 14:43:50 +0700

    dev: base

--- src/app/dashboard/crud/modal.tsx
+++ src/app/dashboard/crud/modal.tsx
@@ -36,9 +36,9 @@ const Modal = ({ hideModal, showModal, parseData }: ModalProps) => {
                     type="submit"
                     className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
             px-4 py-3 mt-6"
-            onClick={parseData}
+            onClick={hideModal}
                 >
-                    Log In
+                    submit
                 </button>
             </motion.div>
 
--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -14,24 +14,24 @@ const Sidebar = ({ showSidebar }: { showSidebar: boolean }) => {
             title: "CRUD Page"
         },
         {
-            url: "#",
+            url: "/dashboard/scroll-page",
+            title: "Scroll Page"
+        },
+        {
+            url: "/dashboard/detail/2",
             title: "Detail 2"
         }
     ]
 
-    console.log(showSidebar)
-
     return (
-      showSidebar ? <motion.div
+    <motion.div
             
             initial={{ translateX:"-100%" }}
             animate={{ translateX: 0 }}
             transition={{
                 duration: 0.1,
                 ease: [0, 0.3, 0.5, 1]
-
             }}
-            data-isOpen={showSidebar}
             className={` ${showSidebar ? "hidden md:flex" : "flex md:hidden"}  absolute w-full  md:static md:w-72 flex-col space-y-2 border-r-2 border-gray-200 bg-white p-2`}
             style={{ height: "90.5vh" }}
         >
@@ -40,7 +40,7 @@ const Sidebar = ({ showSidebar }: { showSidebar: boolean }) => {
                     <ItemMenu item={x} key={index} />
                 ))
             }
-        </motion.div> : null
+        </motion.div> 
     )
 }
 
